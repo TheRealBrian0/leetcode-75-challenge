32bf229852e614238ddfc3311f8716b969340779
Arvin Brian <90852499+TheRealBrian0@users.noreply.github.com>
2026-03-25 17:45:40 +0530

    Time: 13 ms (74.01%), Space: 46.5 MB (44.4%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,21 +1,24 @@
 class Solution {
+    public boolean vowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o'|| c=='u';
+    }
     public int maxVowels(String s, int k) {
-        Set<Character> vowels = Stream.of('a','e','i','o','u').collect(Collectors.toSet());     
+        // Set<Character> vowels = Stream.of('a','e','i','o','u').collect(Collectors.toSet());     
         int count = 0;   
         //first 'k' elements
         for(int i=0; i<k; i++){
-            if(vowels.contains(s.charAt(i))){
+            if(vowel(s.charAt(i))){
                 count++;
             }
         }
         int i = k;
         int maxl = count;
         while(i<s.length()){
-            if(vowels.contains(s.charAt(i-k))){
+            if(vowel(s.charAt(i-k))){
                 //if 'leaving' element is a vowel, subtract count
                 count--;
             }
-            if(vowels.contains(s.charAt(i))){
+            if(vowel(s.charAt(i))){
                 //if 'entering' element is a vowel, add to count
                 count++;
             }
