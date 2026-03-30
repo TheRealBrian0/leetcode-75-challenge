44094e7293e95e668a6e1a5bfd77803dbe5af9b6
Arvin Brian <90852499+TheRealBrian0@users.noreply.github.com>
2026-03-30 14:43:01 +0530

    Time: 10 ms (92.52%), Space: 47.7 MB (31.28%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -8,6 +8,8 @@ class Solution {
         out.add(new ArrayList<>());
         for(int i=0; i<nums1.length; i++){
             s1.add(nums1[i]);
+        }
+        for(int i=0; i<nums2.length; i++){
             s2.add(nums2[i]);
         }
         for(int a:s1){
